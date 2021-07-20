d087ba8ade8bbbb667044822b4d99a2467491e62
Khadilkar Prijay <deven.khadilkar@gmail.com>
2021-07-20 19:10:47 +0530

    initial commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 80.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 50.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 80.0,
+);
+const KTextFieldInputDecoration = InputDecoration(
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  filled: true,
+  fillColor: Colors.white,
+  hintText: 'Enter city name',
+  hintStyle: TextStyle(color: Colors.grey),
+  border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+      borderSide: BorderSide.none),
+);
